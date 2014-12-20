5444abe0432bc3f32b542542cd09bc35211dacce
J. Xavier Prochaska <xavier@dhcp-22-101.ucolick.org>
2014-12-19 17:07:10 -0800

    jxp

--- xastropy/xguis/spec_widgets.py
+++ xastropy/xguis/spec_widgets.py
@@ -145,6 +145,7 @@ class ExamineSpecWidget(QtGui.QWidget):
         if replot is True:
             self.ax.clear()        
             self.ax.plot(self.spec.dispersion, self.spec.flux, 'k-',drawstyle='steps-mid')
+            self.ax.plot(self.spec.dispersion, self.spec.sig, 'r:')
             self.ax.set_xlabel('Wavelength')
             self.ax.set_ylabel('Flux')
 
@@ -160,7 +161,7 @@ class ExamineSpecWidget(QtGui.QWidget):
                     wrest = self.llist[self.llist['List']]['wrest'][jj]
                     lbl = self.llist[self.llist['List']]['name'][jj]
                     # Plot
-                    self.ax.plot(wrest*np.array([z+1,z+1]), self.psdict['ymnx'], 'b:')
+                    self.ax.plot(wrest*np.array([z+1,z+1]), self.psdict['ymnx'], 'b--')
                     # Label
                     self.ax.text(wrest*(z+1), ylbl, lbl, color='blue', rotation=90., size='small')
 
@@ -281,7 +282,7 @@ class PlotLinesWidget(QtGui.QWidget):
                 self.statusBar().showMessage('ERROR: z Input must be a float! Try again..')
             except AttributeError:
                 print('ERROR: z Input must be a float! Try again..')
-            self.zbox.setText('{:.5f}'.format(self.llist['z']))
+            self.zbox.setText('{:.6f}'.format(self.llist['z']))
             return
             
         # Report
@@ -424,6 +425,174 @@ class AbsSysWidget(QtGui.QWidget):
         #QtCore.pyqtRemoveInputHook()
         #xdb.set_trace()
         #QtCore.pyqtRestoreInputHook()
+
+class VelPlotWidget(QtGui.QWidget):
+    ''' Widget to plot a spectrum and interactively
+        fiddle about.  Akin to XIDL/x_specplot.pro
+        Generating as a QDialog so it can be launched separately
+
+        12-Dec-2014 by JXP
+    '''
+    def __init__(self, spec, parent=None, status=None, llist=None,
+                 abs_sys=None):
+        '''
+        spec = Spectrum1D
+        '''
+        super(ExamineSpecWidget, self).__init__(parent)
+
+        self.spec = spec
+        self.abs_sys = abs_sys
+        self.psdict = {} # Dict for spectra plotting
+        self.init_spec() 
+
+        # Status Bar?
+        if not status is None:
+            self.statusBar = status
+
+        # Line List?
+        if llist is None:
+            self.llist = {'Plot': False}
+        else:
+            self.llist = llist
+        
+        # Create the mpl Figure and FigCanvas objects. 
+        # 5x4 inches, 100 dots-per-inch
+        #
+        self.dpi = 150
+        self.fig = Figure((8.0, 4.0), dpi=self.dpi)
+        self.canvas = FigureCanvas(self.fig)
+        self.canvas.setParent(self)
+
+        self.canvas.setFocusPolicy( QtCore.Qt.ClickFocus )
+        self.canvas.setFocus()
+        self.canvas.mpl_connect('key_press_event', self.on_key)
+        self.canvas.mpl_connect('button_press_event', self.on_click)
+
+        # Make two plots
+
+        self.ax = self.fig.add_subplot(1,1,1)
+
+        self.fig.subplots_adjust(hspace=0.1, wspace=0.1)
+        
+        vbox = QtGui.QVBoxLayout()
+        vbox.addWidget(self.canvas)
+        
+        self.setLayout(vbox)
+
+        #
+
+        # Draw on init
+        self.on_draw()
+
+    # Setup the spectrum plotting info
+    def init_spec(self):
+        #xy min/max
+        xmin = np.min(self.spec.dispersion)
+        xmax = np.max(self.spec.dispersion)
+        ymed = np.median(self.spec.flux).value
+        ymin = 0. - 0.1*ymed
+        ymax = ymed * 1.5
+        self.psdict['xmnx'] = [xmin,xmax]
+        self.psdict['ymnx'] = [ymin,ymax]
+        self.psdict['sv_xy'] = [ [xmin,xmax], [ymin,ymax] ]
+        
+    # Main Driver
+    def on_key(self,event):
+
+        flg = 0
+        ## NAVIGATING
+        if event.key in ['l','r','b','t','i','o','[',']','W','Z', 'Y']:  # Set left
+            flg = navigate(self.psdict,event)
+        ## DOUBLET
+        if event.key in ['C','M','V','A']:  # Set left
+            wave = set_doublet(self, event)
+            #print('wave = {:g},{:g}'.format(wave[0], wave[1]))
+            self.ax.plot( [wave[0],wave[0]], self.psdict['ymnx'], '--', color='red')
+            self.ax.plot( [wave[1],wave[1]], self.psdict['ymnx'], '--', color='red')
+            flg = 2 # Layer
+        if event.key == 'R': # Clear lines
+            flg = 1 
+
+        if flg==1: # Default is not to redraw
+            self.on_draw()
+        elif flg==2: # Layer (no clear)
+            self.on_draw(replot=False) 
+
+    # Click of main mouse button
+    def on_click(self,event):
+        print('button={:d}, x={:f}, y={:f}, xdata={:f}, ydata={:f}'.format(
+            event.button, event.x, event.y, event.xdata, event.ydata))
+        if event.button == 1: # Draw line
+            self.ax.plot( [event.xdata,event.xdata], self.psdict['ymnx'], ':', color='green')
+            self.on_draw(replot=False) 
+    
+            # Print values
+            try:
+                self.statusBar().showMessage('x,y = {:f}, {:f}'.format(event.xdata,event.ydata))
+            except AttributeError:
+                return
+
+    def on_draw(self, replot=True):
+        """ Redraws the figure
+        """
+        #
+
+        if replot is True:
+            self.ax.clear()        
+            self.ax.plot(self.spec.dispersion, self.spec.flux, 'k-',drawstyle='steps-mid')
+            self.ax.plot(self.spec.dispersion, self.spec.sig, 'r:')
+            self.ax.set_xlabel('Wavelength')
+            self.ax.set_ylabel('Flux')
+
+            # Spectral lines?
+            if self.llist['Plot'] is True:
+                ylbl = self.psdict['ymnx'][1]-0.2*(self.psdict['ymnx'][1]-self.psdict['ymnx'][0])
+                z = self.llist['z']
+                wvobs = np.array((1+z) * self.llist[self.llist['List']]['wrest'])
+                gdwv = np.where( (wvobs > self.psdict['xmnx'][0]) &
+                                 (wvobs < self.psdict['xmnx'][1]))[0]
+                for kk in range(len(gdwv)): 
+                    jj = gdwv[kk]
+                    wrest = self.llist[self.llist['List']]['wrest'][jj]
+                    lbl = self.llist[self.llist['List']]['name'][jj]
+                    # Plot
+                    self.ax.plot(wrest*np.array([z+1,z+1]), self.psdict['ymnx'], 'b--')
+                    # Label
+                    self.ax.text(wrest*(z+1), ylbl, lbl, color='blue', rotation=90., size='small')
+
+            # Abs Sys?
+            if not self.abs_sys is None:
+                ylbl = self.psdict['ymnx'][0]+0.2*(self.psdict['ymnx'][1]-self.psdict['ymnx'][0])
+                clrs = ['red', 'green', 'cyan', 'orange', 'gray', 'yellow']
+                for abs_sys in self.abs_sys:
+                    ii = self.abs_sys.index(abs_sys)
+                    wrest = np.array(abs_sys.lines.keys()) 
+                    wvobs = wrest * (abs_sys.zabs+1)
+                    gdwv = np.where( ((wvobs+5) > self.psdict['xmnx'][0]) &  # Buffer for region
+                                    ((wvobs-5) < self.psdict['xmnx'][1]))[0]
+                    for kk in range(len(gdwv)): 
+                        jj = gdwv[kk]
+                        #QtCore.pyqtRemoveInputHook()
+                        #xdb.set_trace()
+                        #QtCore.pyqtRestoreInputHook()
+                        # Paint spectrum red
+                        wvlim = wvobs[jj]*(1 + abs_sys.lines[wrest[jj]].analy['VLIM']/3e5)
+                        pix = np.where( (self.spec.dispersion > wvlim[0]) & (self.spec.dispersion < wvlim[1]))[0]
+                        self.ax.plot(self.spec.dispersion[pix], self.spec.flux[pix], '-',drawstyle='steps-mid',
+                                     color=clrs[ii])
+                        # Label
+                        lbl = abs_sys.lines[wrest[jj]].analy['IONNM']+' z={:g}'.format(abs_sys.zabs)
+                        self.ax.text(wvobs[jj], ylbl, lbl, color=clrs[ii], rotation=90., size='x-small')
+        
+        # Reset window limits
+        self.ax.set_xlim(self.psdict['xmnx'])
+        self.ax.set_ylim(self.psdict['ymnx'])
+
+
+
+        # Draw
+        self.canvas.draw()
+    
 
 
 # ######
