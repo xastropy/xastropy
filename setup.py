b272ba7057a3bdda3b8bc6997e2aa8184b882c6c
J. Xavier Prochaska <xavier@ucolick.org>
2015-03-14 08:12:32 -0700

    jxp

--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@ from setuptools import setup
 setup(
     name="xastropy",
     version="0.3.0",
-    author="ProfX",
+    description="Python software for Prof X and associates",
+    author="Prof X",
+    author_email="xavier@ucolick.org",
+    url="https://github.com/profxj/xastropy",
     packages=['xastropy'],
 )
     
