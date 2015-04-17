4da554cd2deb342ac7b3f0f5afbe450938faee72
J. Xavier Prochaska <xavier@ucolick.org>
2015-04-17 16:19:17 -0700

    jxp

--- xastropy/igm/fN/__init__.py
+++ xastropy/igm/fN/__init__.py
@@ -1,2 +1,13 @@
+# Non-dependent modules
 import data
-import model
+
+# Dependent modules
+try:
+    import pymc
+except ImportError:
+    print('-----------------------------------------------------------')
+    print('-----------------------------------------------------------')
+    print('WARNING: Not loading model in xastropy.fN   \n Install pymc if you want it')
+    print('-----------------------------------------------------------')
+else:
+    import model
