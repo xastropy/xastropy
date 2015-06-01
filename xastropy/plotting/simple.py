97c15206656dd64269b99f290727c59fda0420ca
J. Xavier Prochaska <xavier@ucolick.org>
2015-06-01 14:04:51 -0700

    jxp

--- xastropy/plotting/simple.py
+++ xastropy/plotting/simple.py
@@ -223,10 +223,45 @@ def plot_hist(*args,**kwargs):
 
     return
     
+#### ###############################
+#Inset (stolen from http://stackoverflow.com/questions/
+    #    17458580/embedding-small-plots-inside-subplots-in-matplotlib )
+def add_subplot_axes(ax, rect, axisbg='w'):
+    """
+    Add an inset to a subplot.
+    This can run slowly..
 
-
-
-
+    Parameters
+    ----------
+    ax: Axis
+      matplotlib subplot
+    rect: list of 4 floats
+      x, y, Dx, Dy of the inset
+    """
+    fig = plt.gcf()
+    box = ax.get_position()
+    width = box.width
+    height = box.height
+    #
+    inax_position  = ax.transAxes.transform(rect[0:2])
+    transFigure = fig.transFigure.inverted()
+    infig_position = transFigure.transform(inax_position)    
+    x = infig_position[0]
+    y = infig_position[1]
+    #
+    width *= rect[2]
+    height *= rect[3]  # <= Typo was here
+    subax = fig.add_axes([x,y,width,height],axisbg=axisbg)
+    # Fonts
+    x_labelsize = subax.get_xticklabels()[0].get_size()
+    y_labelsize = subax.get_yticklabels()[0].get_size()
+    x_labelsize *= rect[2]**0.5
+    y_labelsize *= rect[3]**0.5
+    #
+    subax.xaxis.set_tick_params(labelsize=x_labelsize)
+    subax.yaxis.set_tick_params(labelsize=y_labelsize) 
+    # Return
+    return subax
 
 
 ## #################################    
