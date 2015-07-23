ebf84fb90aa4069fc34bf02bb92e2da6e8d9fef3
J. Xavier Prochaska <xavier@ucolick.org>
2015-07-22 17:39:17 -0700

    jxp

--- xastropy/xutils/ginga.py
+++ xastropy/xutils/ginga.py
@@ -22,7 +22,7 @@ from astropy.table import QTable, Table, Column
 
 from ginga.util import grc
 
-from xastropy.xutils import xdebug as xdb
+#from xastropy.xutils import xdebug as xdb
 
 # def bintab_to_table(fits_fil,exten=1, silent=False):
 # def table_to_fits(table, outfil, compress=False, comment=None):
@@ -40,16 +40,41 @@ def show_fits(fits_file, host='localhost', port=9000, **kwargs):
     port: int, optional
       Value of the port; Default=9000
     ''' 
+    # Full path?
+    if fits_file[0] != '/':
+        fil = os.getcwd()+'/'+fits_file
+    else:
+        fil = fits_file
     # Check for file
-    if not os.path.isfile(fits_file) == 0:
-        raise ValueError('File={:s} not found!'.format(fits_file))
-    # Connect to ginga RC
+    if not os.path.isfile(fil):
+        raise ValueError('File={:s} not found!'.format(fil))
+    # Connect to ginga RC (should have error checking here)
     client = grc.RemoteClient(host, port)
     # Connect to ginga widget
     method = getattr(client, 'ginga')
     # Set up args 
     command = 'load_file'
-    args = [command,fits_file]
-    # 
+    args = [command,fil]
+    # Execute
     res = method(*args, **kwargs)
 
+def show_img(img,tmp_dir='./',tmp_file='tmp_ginga.fits', **kwargs):
+    '''Push an image to an Ginga window
+    Currently uses the kludge of writing a temp file
+    '''
+    # Generate filename with full path
+    if tmp_file[0] != '/':
+        kludge_fil = os.getcwd()+'/'+tmp_dir+tmp_file
+    else:
+        kludge_fil = tmp_file
+    # Error checking
+    if len(img.shape) != 2:
+        raise ValueError('Image is wrong shape!')
+    # Generate hdu
+    hdu = fits.PrimaryHDU(img)
+    hdulist = fits.HDUList([hdu])
+    # Write
+    print('WARNING: Writing kludge file to {:s}'.format(kludge_fil))
+    hdulist.writeto(kludge_fil,clobber=True)
+    # Show
+    show_fits(kludge_fil, **kwargs)
--- xastropy/xutils/xdebug.py
+++ xastropy/xutils/xdebug.py
@@ -14,6 +14,8 @@ from pdb import *
 from xastropy.xutils.printing import printcol as xpcol
 from xastropy.plotting.simple import plot_1d_arrays as xplot
 from xastropy.plotting.simple import plot_hist as xhist
+from xastropy.xutils.ginga import show_fits as xshow_fits
+from xastropy.xutils.ginga import show_img as ximshow
 
 """ Didn't work
 def pyqt_trace():
