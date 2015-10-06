25f22a2f084fdae6579728e6b2293eda38bafb17
J. Xavier Prochaska <xavier@ucolick.org>
2015-10-06 06:19:26 -0700

    small change to navigate

--- xastropy/xutils/xdebug.py
+++ xastropy/xutils/xdebug.py
@@ -14,6 +14,7 @@ from pdb import *
 from xastropy.xutils.printing import printcol as xpcol
 from xastropy.plotting.simple import plot_1d_arrays as xplot
 from xastropy.plotting.simple import plot_hist as xhist
+#from xastropy.xguis.spec_guis import run_xspec as xspec
 
 try:
 	from xastropy.xutils.xginga import show_fits as xshow_fits
