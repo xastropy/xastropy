a2ee8cff9244dd865553354be664a6bb3ebe2ab1
profxj <xavier@ucolick.org>
2017-07-20 09:49:16 -0700

    clear init

--- xastropy/xutils/__init__.py
+++ xastropy/xutils/__init__.py
@@ -1,16 +1,1 @@
-from . import arrays
-from . import files
-from . import fits
-from . import math
-from . import printing
-from . import xdebug
-try:
-    import ginga
-except ImportError:
-    print('-----------------------------------------------------------')
-    print('-----------------------------------------------------------')
-    print('WARNING: Not loading ginga model in xastropy.utils   \n Install ginga if you want it')
-    print('-----------------------------------------------------------')
-else:
-    import xginga
 
